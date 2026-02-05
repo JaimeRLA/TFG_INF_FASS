ff7c976c053823d81b1d0c42bf089c4ef99ba66f
JaimeRLA <jaime_ruiz@Jaime.localdomain>
2026-02-05 12:25:42 +0100

    Updated url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -59,7 +59,7 @@ const App = () => {
       return;
     }
     try {
-      const res = await axios.post('https://tfg-inf-fass.onrender.com', {
+      const res = await axios.post('https://tfg-inf-fass.onrender.com/calculate', {
         nombre: paciente.nombre,
         paciente_id: paciente.id,
         sintomas: listaIds
